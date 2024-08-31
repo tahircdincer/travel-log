372a5609b4d5b0c757e2ffdc213b06fd25e97aa4
Tahir Dincer <coskun.dincer2014@gmail.com>
2024-09-01 02:21:59 +0300

    ' hard ' coding a card so I can apply props on it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import data from "./data";
 import Navbar from "./components/Navbar";
+import Card from "./components/Card";
 function App() {
   return (
     <div>
       <Navbar />
+      <Card />
     </div>
   );
 }
